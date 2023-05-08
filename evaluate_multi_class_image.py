fixed the training script for multi-class training and evaluation. Fixed the CLIP embeddings computation

evaluate_multi_class_image.py
@@ -8,6 +8,8 @@ import os
 import glob
 import torch
 import pickle
+import numpy as np
+
 from tqdm import tqdm
 from diffusers import StableDiffusionPipeline
 from seg_module import Segmodule
@@ -15,12 +17,12 @@ from utils import preprocess_mask, get_embeddings, calculate_iou
 
 
 batch_size = 1
-grounding_checkpoint = "checkpoints/run-May04_21-07-32/checkpoint_100.pth"
+grounding_checkpoint = "checkpoints/run-May08_13-07-43/checkpoint_1000.pth"
 device = torch.device("cuda")
 model_name = "runwayml/stable-diffusion-v1-5"
-data_path = "dataset/samples/"
+data_path = "data/sd1-5-unseen/samples/"
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # Load the segmentation module
 seg_module = Segmodule().to(device)
@@ -36,11 +38,16 @@ pipeline_components = pipeline.components
 tokenizer = pipeline_components["tokenizer"]
 embedder = pipeline_components["text_encoder"]
 
+# Compute the token_id -> token text mapping
+tokenizer_inverted_vocab = {
+    v: k for k, v in tokenizer.get_vocab().items()
+}
+
 test_samples = glob.glob(data_path + "*.pk")
 total_steps = len(test_samples)
 
 if total_steps == 0:
-    print(
+    raise ValueError(
         f"{data_path} does not contain any data. "
         "make sure you added a trailing / to the data_path"
     )
@@ -49,6 +56,8 @@ seg_module.eval()
 
 with torch.no_grad():
     # Do a single pass over all the data sample
+    iou_scores = []
+
     for step, file_path in enumerate(tqdm(test_samples)):
         image_path = test_samples[step]
 
@@ -57,28 +66,34 @@ with torch.no_grad():
 
         # Unpack the sample data
         labels = sample.labels
-        segmentation = sample.masks
+        segmentations = sample.masks
         unet_features = sample.unet_features
 
         # Move the UNet features to cpu
         for key in unet_features.keys():
             unet_features[key] = [x.to(device) for x in unet_features[key]]
 
-        total_iou = 0
-        for label in range(len(labels)):
-            prompt_embeddings = get_embeddings(
-                tokenizer=tokenizer,
-                embedder=embedder,
-                device=device,
-                prompt=f"a photograph of a {label}",
-                batch_size=batch_size,
-            )
+        sample_iou = []
 
-            # Predict the mask using the fusion module
-            fusion_segmentation = seg_module(unet_features, prompt_embeddings)
+        prompt = " and a ".join(labels)
+        label_embeddings = get_embeddings(
+            tokenizer=tokenizer,
+            embedder=embedder,
+            device=device,
+            prompt=prompt,
+            labels=labels,
+            inverted_vocab=tokenizer_inverted_vocab
+        )
+    
+        for label, segmentation in zip(labels, segmentations):
+            fusion_segmentation = seg_module(unet_features, label_embeddings[label])
             fusion_segmentation_pred = fusion_segmentation[0, 0, :, :]
             fusion_mask = preprocess_mask(mask=fusion_segmentation_pred.unsqueeze(0))
-            iou = calculate_iou(segmentation[label], fusion_mask)
-            total_iou += iou
 
-        print(iou)
+            sample_iou.append(calculate_iou(segmentation, fusion_mask))
+
+        iou_scores.append(np.array(sample_iou).mean())
+
+    mean_iou = np.array(iou_scores).mean()
+
+    print(f"the mean IoU across the dataset at {data_path} using the checkpoint {grounding_checkpoint} is {mean_iou}")

utils/__init__.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 
 from enum import Enum
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 
 from diffusers import StableDiffusionPipeline
 from grounded_unet import GroundedUNet2DConditionModel
@@ -111,16 +111,79 @@ def load_stable_diffusion(
     return StableDiffusionPipeline(**pipeline_components), grounded_unet
 
 
-def get_embeddings(tokenizer, embedder, device: torch.device, prompt: str, batch_size: int):
-  tokens = tokenizer(prompt, return_tensors="pt")
+def token_indices_for_labels(prompt_tokens: Dict[str, torch.Tensor], labels: List[str], inverted_vocab: dict):
+    """
+        Given the tokens of a prompt and a list of labels it returns
+        a mapping from each label to the list of associated token ids,
+        for example:
+        
+        {
+            "pottedplant": [3547, 4841, 3912]
+        }
 
-  tokens["input_ids"] = tokens["input_ids"].to(device)
-  tokens["attention_mask"] = tokens["attention_mask"].to(device)
+        This is because the tokenizer splits `pottedplant` into three
+        tokens: pot, ted, plant
+    """
+    # Map the token ids to tuples in the form (text_token, input_id)
+    text_tokens = [
+        (inverted_vocab[input_id].replace("</w>", ""), input_id)
+        for input_id in prompt_tokens["input_ids"].squeeze().tolist()
+    ]
 
-  token_embeddings = embedder(**tokens).last_hidden_state
-  token_embeddings = token_embeddings[:, len(tokens["input_ids"]), :].to(device)
+    # Remove non-relevant tokens
+    ignored_tokens = {"<|startoftext|>", "a", "photograph", "of", "a", "<|endoftext|>", "and"}
+    text_tokens = [token for token in text_tokens if token[0] not in ignored_tokens]
 
-  return token_embeddings.repeat(batch_size, 1, 1)
+    # Create a mapping label -> input_ids for each label
+    token_ids_mapping = {}
+
+    for label in labels:
+        token_ids_mapping[label] = [
+            token[1] for token in text_tokens if token[0] in label
+        ]
+
+    return token_ids_mapping
+
+
+def get_embeddings(
+    tokenizer,
+    embedder,
+    device: torch.device,
+    prompt: str,
+    labels: List[str],
+    inverted_vocab: dict
+):
+    tokens = tokenizer(prompt, return_tensors="pt")
+
+    # Then in the other function we get the embeddings for those words and average
+    # them together if they use multiple tokens
+    token_ids_mapping = token_indices_for_labels(
+        prompt_tokens=tokens,
+        labels=labels,
+        inverted_vocab=inverted_vocab
+    )
+
+    tokens["input_ids"] = tokens["input_ids"].to(device)
+    tokens["attention_mask"] = tokens["attention_mask"].to(device)
+
+    input_ids_list = tokens["input_ids"]
+    token_embeddings = embedder(**tokens).last_hidden_state
+    label_embeddings = {}
+
+    for label in token_ids_mapping.keys():
+        label_token_indices = torch.Tensor([
+            (input_ids_list == token_id).nonzero(as_tuple=True)[1]
+            for token_id in token_ids_mapping[label]
+        ]).long()
+
+        # Take the mean of all the embedding related to this label.
+        # for example pottedplant will have three tokens (pot, ted,
+        # plant)
+        label_embeddings[label] = token_embeddings[
+            :, label_token_indices, :
+        ].mean(dim=1, keepdim=True).to(device)
+
+    return label_embeddings
 
 
 def has_mask_for_classes(masks: List[List], class_indices: List[int]) -> bool: