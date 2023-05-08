238457d31e57c9cf484eebcaa723a203ef7c497c
Ankit Ankit <lcur0899@test3.irc.sara.nl.irc.sara.nl>
2023-05-08 20:30:41 +0200

    added iou check

--- generate_dataset.py
+++ generate_dataset.py
@@ -7,13 +7,14 @@ import numpy as np
 
 from data import MultiClassSegmentationSample
 from mmdet.apis import init_detector, inference_detector
-from utils import load_stable_diffusion, has_mask_for_classes
+from utils import load_stable_diffusion, has_mask_for_classes, calculate_iou
 
 
 # the number of classes in a single sample
 n_classes = 2
-total_samples = 500
-output_dir = "dataset"
+total_samples = 5
+output_dir = "/scratch/data_sd1/"
+# output_dir = "/home/lcur0899/save_samples/test5/"
 pascal_class_split = 1
 device = torch.device("cuda")
 model_name = "runwayml/stable-diffusion-v1-5"
@@ -22,11 +23,11 @@ model_type = model_name.split("/")[-1]
 images_dir = os.path.join(output_dir, "images")
 samples_dir = os.path.join(output_dir, "samples")
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 mask_rnn_config = {
-  "config": "mmdetection/configs/swin/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py",
-  "checkpoint": "mmdetection/checkpoint/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco_20210903_104808-b92c91f1.pth"
+    "config": "mmdetection/configs/swin/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py",
+    "checkpoint": "mmdetection/checkpoint/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco_20210903_104808-b92c91f1.pth",
 }
 
 os.makedirs(output_dir, exist_ok=True)
@@ -44,9 +45,7 @@ train_classes, test_classes = pascal_classes[:15], pascal_classes[15:]
 
 # Load Mask R-CNN
 pretrain_detector = init_detector(
-  mask_rnn_config["config"],
-  mask_rnn_config["checkpoint"],
-  device=device
+    mask_rnn_config["config"], mask_rnn_config["checkpoint"], device=device
 )
 
 execution_time = int(time.time())
@@ -77,24 +76,21 @@ for i in range(total_samples):
         unet_features[key] = [x.to("cpu") for x in unet_features[key]]
 
     # Get the segmentation
-    _, segmentation = inference_detector(
-        pretrain_detector,
-        [array_image]
-    ).pop()
+    _, segmentation = inference_detector(pretrain_detector, [array_image]).pop()
 
-    has_masks = has_mask_for_classes(
-        masks=segmentation,
-        class_indices=class_indices
-    )
+    has_masks = has_mask_for_classes(masks=segmentation, class_indices=class_indices)
 
     if not has_masks:
         continue
 
     segmented_classes = [
-        segmentation[class_index][0].astype(int)
-        for class_index in class_indices
+        segmentation[class_index][0].astype(int) for class_index in class_indices
     ]
 
+    checkiou = calculate_iou(segmented_classes[0], segmented_classes[1])
+    if checkiou > 0.95:
+        continue
+    print("check iou : ", checkiou)
     # For each sample we want to save
     #
     # the generated image
@@ -105,12 +101,11 @@ for i in range(total_samples):
         image=array_image,
         masks=segmented_classes,
         unet_features=unet_features,
-        labels=picked_classes
+        labels=picked_classes,
     )
 
     image.save(os.path.join(images_dir, f"{execution_time}_{i}.png"))
 
     pickle.dump(
-        sample,
-        open(os.path.join(samples_dir, f"{execution_time}_{i}.pk"), "wb")
+        sample, open(os.path.join(samples_dir, f"{execution_time}_{i}.pk"), "wb")
     )
