9f93e07700490c492dda337a350ce66f8463be3c
Ankit Ankit <lcur0899@test3.irc.sara.nl.irc.sara.nl>
2023-05-15 15:44:03 +0200

    added scoring to the code

--- seg_utils/evaluation.py
+++ seg_utils/evaluation.py
@@ -1,17 +1,25 @@
+
+import os 
 import torch
 import pickle
 import numpy as np
-
+from PIL import Image
 from tqdm import tqdm
 from typing import List
 from .prompts import get_embeddings
 from .segmentation import preprocess_mask, calculate_iou
+import torchvision.transforms as transforms
+
+
+transform = transforms.ToTensor()
 
 
 def evaluate_seg_model(
     model,
+    extractor,
     tokenizer,
     embedder,
+    phase_images_path,
     device: torch.device,
     tokenizer_inverted_vocab: dict,
     samples_paths: List[str]
@@ -21,12 +29,19 @@ def evaluate_seg_model(
     for _, file_path in enumerate(tqdm(samples_paths)):
         with open(file_path, "rb") as sample_file:
             sample = pickle.load(sample_file)
+        
+        image_file = file_path.split("/")[-1].replace(".pk", ".png")
+        full_image_phase_path = os.path.join(phase_images_path, image_file)
+        phase_open_image = Image.open(full_image_phase_path)
+
+        with torch.no_grad():
+            image = extractor.preprocess(transform(phase_open_image), 512)
+            saliency_map = extractor.extract_saliency_maps(image.to(device))
 
         # Unpack the sample data
         labels = sample.labels
         segmentations = sample.masks
         unet_features = sample.unet_features
-        saliency_map = sample.dino_saliency_map.to(device)
 
         # Move the UNet features to cpu
         for key in unet_features.keys():
@@ -48,7 +63,7 @@ def evaluate_seg_model(
             fusion_segmentation = model(unet_features, label_embeddings[label], saliency_map)
             fusion_segmentation_pred = fusion_segmentation[0, 0, :, :]
             fusion_mask = preprocess_mask(mask=fusion_segmentation_pred.unsqueeze(0))
-
+            
             sample_iou.append(calculate_iou(segmentation, fusion_mask))
 
         iou_scores.append(np.array(sample_iou).mean())
